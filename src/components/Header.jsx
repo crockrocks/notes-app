5a82d40a4087e3d7c674f1936f73295aa2ed065e
Your Name <harshpant3703@gmail.com>
2023-09-02 17:19:58 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Notes <i class="fa-solid fa-pencil"></i>...</h1>
+    </header>
+  );
+}
+
+export default Header;
